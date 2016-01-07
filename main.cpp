72b87952c0a3acad200a3a651ceeda10268254ac
darryl <darryl@darryl-Satellite-S55D-A>
2016-01-07 15:22:22 -0800

    asdf

--- main.cpp
+++ main.cpp
@@ -14,9 +14,15 @@ using namespace std;
 
 rect getBB(triangle t);
 
+double areatri(vertex a, vertex b, vertex c);
+
 int main(int argc, char *argv[]) {
   // make a color
   color_t clr;
+  color_t clra;
+  color_t clrb;
+  color_t clrc;
+  color_t clrt;
 
   cout << "Input 3 vertices: \n";
 
@@ -44,25 +50,68 @@ int main(int argc, char *argv[]) {
   tri.b = b;
   tri.c = c;
 
+
+
   bb = getBB(tri);
 
   clr.r = 0.1;
   clr.g = 0.5;
   clr.b = 0.9;
 
+  clra.r = 1;
+  clra.g = 0;
+  clra.b = 0;
+
+  clrb.r = 0;
+  clrb.g = 1;
+  clrb.b = 0;
+
+  clrc.r = 0;
+  clrc.g = 0;
+  clrc.b = 1;
+
+
+
+
   // make a 640x480 image (allocates buffer on the heap)
   Image img(640, 480);
-
+  double totalarea = areatri(tri.a, tri.b, tri.c);
+  cout << totalarea;
   // set a square to be the color above
   for (int i=bb.minx; i < bb.maxx; i++) {
     for (int j=bb.miny; j < bb.maxy; j++) {
-      img.pixel(i, j, clr);
+    	vertex test;
+    	test.x = i;
+    	test.y = j;
+    	test.z = 0;
+    	double alpha = areatri(tri.a, tri.b, test)/totalarea;
+    	double beta = areatri(tri.c, tri.a, test)/totalarea;
+    	double gamma = 1 - alpha - beta;
+
+
+
+    	if (alpha >= 0 && alpha <=1 && beta >= 0 && beta <=1 && gamma >= 0 && gamma <=1) {
+    		clrt.r = ((alpha * clrc.r) + (beta * clrb.r) + (gamma * clra.r))/3 ;
+    		clrt.g = ((alpha * clrc.g) + (beta * clrb.g) + (gamma * clra.g))/3 ;
+    		clrt.b = ((alpha * clrc.b) + (beta * clrb.b) + (gamma * clra.b))/3 ;
+
+    		img.pixel(i,j,clrt);
+
+    	}
+
+    	else
+    		img.pixel(i, j, clr);
     }
   }
 
   // write the targa file to disk
   img.WriteTga((char *)"awesome.tga", true); 
   // true to scale to max color, false to clamp to 1.0
+
+}
+
+double areatri(vertex a, vertex b, vertex c) {
+	return ((b.x-a.x)*(c.y - a.y) - (c.x-a.x) * (b.y-a.y))/2;
 
 }
 
